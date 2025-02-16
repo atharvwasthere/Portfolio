ad58ae349e8c5a0f83b385f277de3ff2b01915c1
Atharv-Singh-Git <singhatharv1919@gmail.com>
2025-02-17 03:33:13 +0530

    fix: Updated About  Section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,20 +1,22 @@
 import { AboutMe } from '../data/Data'
-import { DotLottieReact } from '@lottiefiles/dotlottie-react';
-import AnimatedTerminal from './terminal-animation';
-import aboutgif from '../data/video (1).gif';
+import MagnetLines from './blocks/Animations/MagnetLines/MagnetLines';
+import { useTheme } from './theme-provider';
+
 const About = () => {
 
+    const { theme, resolvedTheme } = useTheme();
+    const currentTheme = theme === "system" ? resolvedTheme : theme;
+    const currentLineColor = currentTheme === "dark" ? "white" : "#000";
     return (
         <>
-            <div id="main-outer-border" className="  grid grid-cols-1 row-2 md:grid-cols-2 gap-2 border-2 border-red-500">
-                <div className="grid  text-primary items-center justify-center  border-2 border-red-500">
+            <div id="main-outer-border" className="  grid grid-cols-1 row-2 md:grid-cols-2 gap-2">
+                <div className="grid  text-primary items-center justify-center ">
                     <div id="pic" className='grid items-center justify-center '>
-                        {/* <DotLottieReact src="src/data/About.lottie" loop autoplay className='-pt-8'></DotLottieReact> */}
-                        {/* <AnimatedTerminal /> */}
-                        <img src={aboutgif} />
+                        {/* <img src={aboutgif} /> */}
+                        <MagnetLines lineColor={currentLineColor}/>
                     </div>
                 </div>
-                <div className="grid pt-24 justify-center text-start text-primary text-small">
+                <div className="grid pt-24  justify-between text-start text-primary text-small">
                     <p className='pb-24 '>{AboutMe.content()}</p>
 
                 </div>
