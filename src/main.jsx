5905f7c502edd97db7c20f555a84f567c851048a
Atharvwasthere <singhatharv1919@gmail.com>
2025-09-05 23:32:54 +0530

    refactor: remove unused font files and clean up imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,29 @@
-// main.jsx
 import { createRoot } from "react-dom/client"
-import { BrowserRouter, Routes, Route } from "react-router"
+import { BrowserRouter, Routes, Route } from "react-router-dom" 
 import { lazy, Suspense } from "react"
 import "./index.css"
 import App from "./App.jsx"
-import "./segment.css"
-import "./satoshi.css"
-import "./cabinetGrotesk.css"
-import "./zodiak.css"
-import "./autography.css"
 import { ThemeProvider } from "./components/theme-provider"
 
 // Lazy imports
 const BlogSection = lazy(() => import("./components/Blogs"))
 const ProjectPage = lazy(() => import("./pages/ProjectPage"))
 
-createRoot(document.getElementById("root")).render(
-  <ThemeProvider>
-    <BrowserRouter>
-      <Suspense fallback={<div className="p-4">Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/Blogs" element={<BlogSection />} />
-          <Route path="/project/:slug" element={<ProjectPage />} />
-        </Routes>
-      </Suspense>
-    </BrowserRouter>
-  </ThemeProvider>
-)
+// Exporting at least one component so Fast Refresh can track it
+export function Root() {
+  return (
+    <ThemeProvider>
+      <BrowserRouter>
+        <Suspense fallback={<div className="p-4">Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/Blogs" element={<BlogSection />} />
+            <Route path="/project/:slug" element={<ProjectPage />} />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </ThemeProvider>
+  )
+}
+
+createRoot(document.getElementById("root")).render(<Root />)
